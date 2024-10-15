6c0bccea3ceb0efa7b5a56973046caea3a2999ad
hakam tamlani <135169366+hakamtamlani@users.noreply.github.com>
2024-10-15 16:59:36 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://crud-mern-stack-frontend.vercel.app"],
+    origin: ["https://crud-mern-stack-frontend.vercel.app/"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
